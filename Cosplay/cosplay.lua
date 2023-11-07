e339f2685f14908dfec7d97db443e4e1f0a00d16
Timothy Cricchio <cricchio.timothy@gmail.com>
2023-11-06 21:19:21 -0500

    Initial push of cosplay addon

---
+++ Cosplay/cosplay.lua
@@ -0,0 +1,263 @@
+-- Copyright (c) 2023 Tirem
+
+addon.name      = 'Cosplay';
+addon.author    = 'Tirem';
+addon.version   = '1.0';
+addon.desc      = 'Copies the look at the targets gear and applies it to your self';
+addon.link      = 'https://ashitaxi.com/';
+
+require('common');
+local chat = require('chat');
+local ffi = require('ffi');
+
+-- saved outfit this session
+local Cosplay = T{
+};
+
+-- Cached version of what we are supposed to look like
+local OriginalLook = T{
+};
+
+-- Cache player look now if we have one
+local player = GetPlayerEntity();
+if (player ~= nil) then
+    OriginalLook.Head = player.Look.Head;
+    OriginalLook.Body = player.Look.Body;
+    OriginalLook.Hands = player.Look.Hands;
+    OriginalLook.Legs = player.Look.Legs;
+    OriginalLook.Feet = player.Look.Feet;
+end
+
+local function ResetLook()
+    local player = GetPlayerEntity();
+    if (player ~= nil) then
+        player.Look.Head = OriginalLook.Head;
+        player.Look.Body = OriginalLook.Body;
+        player.Look.Hands = OriginalLook.Hands;
+        player.Look.Legs = OriginalLook.Legs;
+        player.Look.Feet = OriginalLook.Feet;
+        player.ModelUpdateFlags = 0x10;
+    end
+end
+
+local function UpdateLook()
+    local player = GetPlayerEntity();
+    if (player ~= nil) then
+        if (Cosplay.Head ~= nil) then
+            player.Look.Head = Cosplay.Head;
+        end
+        if (Cosplay.Body ~= nil) then
+            player.Look.Body = Cosplay.Body;
+        end
+        if (Cosplay.Hands ~= nil) then
+            player.Look.Hands = Cosplay.Hands;
+        end
+        if (Cosplay.Legs ~= nil) then
+            player.Look.Legs = Cosplay.Legs;
+        end
+        if (Cosplay.Feet ~= nil) then
+            player.Look.Feet = Cosplay.Feet;
+        end
+        player.ModelUpdateFlags = 0x10;
+    end
+end
+
+local function GetStPartyIndex()
+    local ptr = AshitaCore:GetPointerManager():Get('party');
+    ptr = ashita.memory.read_uint32(ptr);
+    ptr = ashita.memory.read_uint32(ptr);
+    local isActive = (ashita.memory.read_uint32(ptr + 0x54) ~= 0);
+    if isActive then
+        return ashita.memory.read_uint8(ptr + 0x50);
+    else
+        return nil;
+    end
+end
+
+local function GetTargets()
+    local playerTarget = AshitaCore:GetMemoryManager():GetTarget();
+    local party = AshitaCore:GetMemoryManager():GetParty();
+
+    if (playerTarget == nil or party == nil) then
+        return nil, nil;
+    end
+
+    local mainTarget = playerTarget:GetTargetIndex(0);
+    local secondaryTarget = playerTarget:GetTargetIndex(1);
+    local partyTarget = GetStPartyIndex();
+
+    if (partyTarget ~= nil) then
+        secondaryTarget = mainTarget;
+        mainTarget = party:GetMemberTargetIndex(partyTarget);
+    end
+
+    return mainTarget, secondaryTarget;
+end
+
+function IsPlayer(targetEntity)
+    -- Obtain the entity spawn flags..
+
+    local flag = targetEntity.SpawnFlags;
+
+    -- Determine the entity type and apply the proper color
+    if (bit.band(flag, 0x0001) == 0x0001) then --players
+		return true;
+    else
+        return false;
+    end
+end
+
+local function CopyTargetsLook()
+    -- Obtain the player target entity (account for subtarget)
+	local targetIndex, _ = GetTargets();
+	local player = GetEntity(targetIndex);
+    if (player ~= nil and IsPlayer(player)) then
+        Cosplay.Head = player.Look.Head;
+        Cosplay.Body = player.Look.Body;
+        Cosplay.Hands = player.Look.Hands;
+        Cosplay.Legs = player.Look.Legs;
+        Cosplay.Feet = player.Look.Feet;
+        return true;
+    end
+    return false;
+end
+
+ashita.events.register('command', 'command_cb', function (e)
+    -- Parse the command arguments..
+    local args = e.command:args();
+    if (#args == 0 or (args[1] ~= '/cosplay' and args[1] ~= '/cos')) then
+        return;
+    end
+
+    -- Block all related commands..
+    e.blocked = true;
+
+    -- Handle: /Cosplay - Copies the targets current look
+    if (#args == 1) then
+        if (CopyTargetsLook() == true) then
+            UpdateLook();
+            print(chat.header(addon.name):append(chat.message('Cosplaying Target!')));
+        end
+        return;
+    end
+
+    -- Handle: /Cosplay (clear | off) - Turns off Cosplay.
+    if (#args == 2 and args[2]:any('clear', 'off')) then
+        Cosplay.Head = nil;
+        Cosplay.Body = nil;
+        Cosplay.Hands = nil;
+        Cosplay.Legs = nil;
+        Cosplay.Feet = nil;
+        ResetLook();
+        print(chat.header(addon.name):append(chat.message('Cleared Cosplay!')));
+        return;
+    end
+
+    -- Handle: /Cosplay (help) - Turns off Cosplay.
+    if (#args == 2 and args[2]:any('help')) then
+        print(chat.header(addon.name):append(chat.message('"/cosplay" to copy your targets look (Players only)')));
+        print(chat.header(addon.name):append(chat.message('"/cosplay clear" OR "/cosplay off" to clear your cosplay')));
+        print(chat.header(addon.name):append(chat.message('"/cos" works too :)"')));
+        return;
+    end
+end);
+
+ashita.events.register('packet_in', 'packet_in_cb', function (e)
+    if (e.blocked) then
+        return;
+    end
+
+    -- Packet: Zone Enter
+    if (e.id == 0x000A) then
+        local p = ffi.cast('uint8_t*', e.data_modified_raw);
+
+        -- Save incoming look for clearing
+        OriginalLook.Head = p[0x46];
+        OriginalLook.Body = p[0x48];
+        OriginalLook.Hands = p[0x4A];
+        OriginalLook.Legs = p[0x4C];
+        OriginalLook.Feet = p[0x4E];
+        
+        if (Cosplay.Head ~= nil) then
+            p[0x46] = Cosplay.Head;
+        end
+        if (Cosplay.Body ~= nil) then
+            p[0x48] = Cosplay.Body;
+        end
+        if (Cosplay.Hands ~= nil) then
+            p[0x4A] = Cosplay.Hands;
+        end
+        if (Cosplay.Legs ~= nil) then
+            p[0x4C] = Cosplay.Legs;
+        end
+        if (Cosplay.Feet ~= nil) then
+            p[0x4E] = Cosplay.Feet;
+        end
+    end
+
+    -- Packet: Character Update
+    if (e.id == 0x000D) then
+        local p = ffi.cast('uint8_t*', e.data_modified_raw);
+        local i = struct.unpack('L', e.data_modified, 0x04 + 0x1);
+        if (i == AshitaCore:GetMemoryManager():GetParty():GetMemberServerId(0) and p[0x0A] == 0x1F) then
+
+            -- Save incoming look for clearing
+            OriginalLook.Head = p[0x4A];
+            OriginalLook.Body = p[0x4C];
+            OriginalLook.Hands = p[0x4E];
+            OriginalLook.Legs = p[0x50];
+            OriginalLook.Feet = p[0x52];
+
+            if (Cosplay.Head ~= nil) then
+                p[0x4A] = Cosplay.Head;
+            end
+            if (Cosplay.Body ~= nil) then
+                p[0x4C] = Cosplay.Body;
+            end
+            if (Cosplay.Hands ~= nil) then
+                p[0x4E] = Cosplay.Hands;
+            end
+            if (Cosplay.Legs ~= nil) then
+                p[0x50] = Cosplay.Legs;
+            end
+            if (Cosplay.Feet ~= nil) then
+                p[0x52] = Cosplay.Feet;
+            end
+        end
+    end
+
+    --[[ Packet: Character Jobs
+    if (e.id == 0x001B) then
+        local p = ffi.cast('uint8_t*', e.data_modified_raw);
+        p[0x04] = sexchange.race;
+    end
+    --]]
+
+    -- Packet: Character Appearance
+    if (e.id == 0x0051) then
+        local p = ffi.cast('uint8_t*', e.data_modified_raw);
+
+        -- Save incoming look for clearing
+        OriginalLook.Head = p[0x06];
+        OriginalLook.Body = p[0x08];
+        OriginalLook.Hands = p[0x0A];
+        OriginalLook.Legs = p[0x0C];
+        OriginalLook.Feet = p[0x0E];
+
+        if (Cosplay.Head ~= nil) then
+            p[0x06] = Cosplay.Head;
+        end
+        if (Cosplay.Body ~= nil) then
+            p[0x08] = Cosplay.Body;
+        end
+        if (Cosplay.Hands ~= nil) then
+            p[0x0A] = Cosplay.Hands;
+        end
+        if (Cosplay.Legs ~= nil) then
+            p[0x0C] = Cosplay.Legs;
+        end
+        if (Cosplay.Feet ~= nil) then
+            p[0x0E] = Cosplay.Feet;
+        end
+    end
+end);
---
+++ README.md
@@ -0,0 +1,33 @@
+# Cosplay
+
+An ashita v4 addon to copy the look at your current target!
+
+## Show Your Support ##
+If you would like to show your support for my addon creation consider buying me a coffee! 
+
+[![ko-fi](https://ko-fi.com/img/githubbutton_sm.svg)](https://ko-fi.com/A0A6JC40H)
+
+## Overview
+
+This addon allows you to copy the look of any player you are targeting!
+
+
+## Installation
+* Download the latest release of Cosplay from the panel on the right and extract it.
+* Open up the extracted folder and inside of it will be a directory called `addons`.
+* Copy the addons folder to your `Ashita` folder, merging it with your current `Ashita/addons` folder.
+* To load Cosplay, type `/addon load Cosplay` in your chatbox.
+* If you want to load this addon by default:
+    * Open up the file `default.txt` in the `Ashita/scripts` folder.
+    * Add the following line: `/addon load Cosplay`
+* **NOTE:** You must follow these steps to install the addon. Downloading the code directly will not work without source control software as this repository makes use of git submodules!
+
+## Config
+
+To see the help printout for command type `/cosplay help`, or `/cos help` into the chatbox and commands will be listed in chat.
+
+Type `/cos` or `/cosplay` with another player selected and you will cosplay as them! (You will look like you're wearing their gear on your screen only.)
+
+Type `/cos off` or `/cos clear` to return to your normal look!
+
+As of right now cosplays aren't saved but that may be a feature I add in the future if it is requested.
